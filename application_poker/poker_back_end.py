338585c2aa861b8363160c8244f2b58801924741
Dorian <Dorian@taukuas-MacBook-Pro.local>
2019-08-01 17:59:15 +0300

    aa

---
+++ application_poker/poker_back_end.py
@@ -0,0 +1,17 @@
+import random
+def generate_cards(number):
+    value=["1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    symbol=["♣","♦","♥","♠"]
+
+    gen_card=[]
+
+    for i in range(number):
+        random_value=random.choice(value)
+        random_symbol=random.choice(symbol)
+
+        while (random_value,random_symbol) in gen_card:
+            random_value = random.choice(value)
+            random_symbol = random.choice(symbol)
+
+        gen_card.append((random_value,random_symbol))
+    return gen_card
---
+++ application_poker/routes.py
@@ -0,0 +1,14 @@
+import flask
+from application_poker import app
+import application_poker.poker_back_end
+
+@app.route('/')
+def index():
+    return flask.render_template('test.html')
+
+
+@app.route('/poker')
+def launch_poker_game():
+    cards=application_poker.poker_back_end.generate_cards(4)
+    return flask.render_template('poker.html',cards=cards)
+
