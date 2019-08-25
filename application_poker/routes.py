a366fe6abfc62611b2e3cc7c406b49957825a026
Dorian <Dorian@taukuas-MacBook-Pro.local>
2019-08-25 13:24:26 +0300

    add players

--- application_poker/routes.py
+++ application_poker/routes.py
@@ -2,16 +2,23 @@ import flask
 from application_poker import app
 import application_poker.poker_back_end
 
-@app.route('/')
-def index():
-    return flask.render_template('test.html')
 
 
+
+
+
+players=[{'name':'Mike', 'chips':500},{'name':'Brad','chips':50}]
 @app.route('/poker')
 def launch_poker_game():
     cards=application_poker.poker_back_end.generate_cards(4)
+    return flask.render_template('poker.html',cards=cards,players=players)
 
 
 
-    return flask.render_template('poker.html',cards=cards)
+
+@app.route('/test')
+def test():
+    return flask.render_template('test.html')
+
+
 
