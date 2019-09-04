e8473e049d2e2f39e6af6e9f9e4317aefd7d99d9
Dorian <Dorian@taukuas-MacBook-Pro.local>
2019-09-04 20:30:40 +0300

    version with player form semi advanced

--- application_poker/forms.py
+++ application_poker/forms.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 class ConfigGame(FlaskForm):
     name_1 = StringField('Name', validators=[DataRequired()])
     chips_1 = SelectField('Chips', choices=[('1000', '1000 chips'), ('2000', '2000 chips'), ('3000', '3000 chips')], validators=[DataRequired()])
+    avatar_1 = RadioField('Avatar', choices=[('avatar_1.png','avatar_1.png'),('avatar_2.png','avatar_2.png')])
 
     name_2 = StringField('Name', validators=[DataRequired()])
     chips_2 = SelectField('Chips', choices=[('1000', '1000 chips'), ('2000', '2000 chips'), ('3000', '3000 chips')], validators=[DataRequired()])
@@ -16,5 +17,5 @@ class ConfigGame(FlaskForm):
     chips_4 = SelectField('Chips', choices=[('1000', '1000 chips'), ('2000', '2000 chips'), ('3000', '3000 chips')], validators=[DataRequired()])
 
 
-    #avatar_1 = RadioField('Avatar', choices=['a', 'b','c','d'])
+
     submit = SubmitField('Start the game')
--- application_poker/routes.py
+++ application_poker/routes.py
@@ -1,13 +1,12 @@
 import flask
 from application_poker import app
-import application_poker.poker_back_end
+import application_poker.poker_back_end as back_end
 import application_poker.forms
 
-players=[{'name':'Mike', 'chips':500},{'name':'Brad','chips':50}]
 @app.route('/poker')
 def launch_poker_game():
-    cards=application_poker.poker_back_end.generate_cards(4)
-    return flask.render_template('poker.html',cards=cards,players=players)
+    cards=back_end.generate_cards(4)
+    return flask.render_template('poker.html',cards=cards,players=back_end.players)
 
 
 @app.route("/",  methods=('GET', 'POST'))
@@ -15,7 +14,21 @@ def config_game():
     form=application_poker.forms.ConfigGame()
 
     if form.validate_on_submit():
-        return flask.redirect(flask.url_for(''))
+
+        player=back_end.Player(form.name_1.data,int(form.chips_1.data))
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_2.data,int(form.chips_2.data))
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_3.data,int(form.chips_3.data))
+        back_end.players.append(player)
+
+        player=back_end.Player(form.name_4.data,int(form.chips_4.data))
+        back_end.players.append(player)
+
+
+        return flask.redirect(flask.url_for('launch_poker_game'))
     else:
         return flask.render_template('config.html',form=form)
 
